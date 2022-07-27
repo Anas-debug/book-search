0b69be73db170e0202c52b73cf145bc068d791a0
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-07-27 03:07:34 +0100

    Added logic to our custom hook

--- src/useBookSearch.js
+++ src/useBookSearch.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-export default function useBookSearch() {
+export default function useBookSearch(query, pageNumber) {
+
+    useEffect(() => {
+        axios({});
+    }, []);
   return (
     <div>useBookSearch</div>
   )
