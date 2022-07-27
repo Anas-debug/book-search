127b96f62dfbc57aa8ccc72191f18da0be1502e6
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-07-27 02:45:01 +0100

    Installed axios and added  boilerplate jsx

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <React.Fragment>
       <input type= "text"></input>  
+      <div>Book Title</div>
+      <div>Book Title</div>
+      <div>Book Title</div>
+      <div>Book Title</div>
+      <div>Loading...</div>
+      <div>Error</div>
     </React.Fragment>
   );
 }
