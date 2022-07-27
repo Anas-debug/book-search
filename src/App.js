cff9a8106344756853d56af8e29bc273459a73cf
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-07-27 16:55:49 +0100

    Added useEffect Hooks inside our App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useBookSearch from './useBookSearch';
 import './App.css';
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
+
+  useBookSearch(query, pageNumber);
   return (
     <React.Fragment>
       <input type= "text"></input>  
--- src/useBookSearch.js
+++ src/useBookSearch.js
@@ -3,9 +3,14 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function useBookSearch(query, pageNumber) {
-
     useEffect(() => {
-        axios({});
+        axios({
+            method : "GET",
+            url : "",
+            params: {q: query, page : pageNumber}
+        }).then(res => {
+            console.log(res.data);
+        });
     }, []);
   return (
     <div>useBookSearch</div>
