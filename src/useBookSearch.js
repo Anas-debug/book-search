3cd2c99f86ca33756e2dfe55caaa8dc8d1e73cc0
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-07-29 16:10:29 +0100

    Cancel Token need a pattern for that we can sore that data in avariable defined in the useEffect hook scope.

--- src/useBookSearch.js
+++ src/useBookSearch.js
@@ -4,13 +4,19 @@ import axios from 'axios';
 
 export default function useBookSearch(query, pageNumber) {
     useEffect(() => {
+        let cancel;
         axios({
             method : 'GET',
             url : 'https://openlibrary.org/search.json',
-            params: {q: query, page : pageNumber}
+            params: {q: query, page : pageNumber},
+            cancelToken : new axios.CancelToken((c) => {
+                cancel = c;
+            })
         }).then(res => {
             console.log(res.data);
         });
+
+        return () => cancel();
     }, [query, pageNumber]);
   return (
     <div>useBookSearch</div>
