9bbcdd75a5e28557748ec5fb662b04785af26e9a
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-08-02 04:47:30 +0100

    Removed first bug trailing curly brace

--- src/useBookSearch.js
+++ src/useBookSearch.js
@@ -6,22 +6,21 @@ export default function useBookSearch(query, pageNumber) {
     const [loading, setLoading] = useState("");
 
 
-    useEffect(() => {
-        let cancel;
-        axios({
-            method : 'GET',
-            url : 'https://openlibrary.org/search.json',
-            params: {q: query, page : pageNumber},
-            // cancelToken : new axios.CancelToken((c) => {
-            //     cancel = c
-            // }).then((res) =>{
-            //     console.log(res.data);
-            // }).catch((e) => {
-            //     if(axios.isCancel(e)){
-            //         return e;
-            //     }
-            // })
-    }) , [query, pageNumber]);
+    // useEffect(() => {
+    //     let cancel;
+    //     axios({
+    //         method : 'GET',
+    //         url : 'https://openlibrary.org/search.json',
+    //         params: {q: query, page : pageNumber},
+    //         // cancelToken : new axios.CancelToken((c) => {
+    //         //     cancel = c
+    //         // }).then((res) =>{
+    //         //     console.log(res.data);
+    //         // }).catch((e) => {
+    //         //     if(axios.isCancel(e)){
+    //         //         return e;
+    //         //     }
+    //         // })
+    // }) , [query, pageNumber]);
     return(<div>Hello</div>);
-}
 }
