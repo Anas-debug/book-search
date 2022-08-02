409e01eba575eea7260159ab4d1c371425db4cf1
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-08-02 04:45:29 +0100

    debuging useBookSearch Hook

--- src/useBookSearch.js
+++ src/useBookSearch.js
@@ -3,19 +3,24 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function useBookSearch(query, pageNumber) {
+    const [loading, setLoading] = useState("");
+
+
     useEffect(() => {
         let cancel;
         axios({
             method : 'GET',
             url : 'https://openlibrary.org/search.json',
             params: {q: query, page : pageNumber},
-            cancelToken : new axios.CancelToken((c) => {
-                cancel = c
-            }).then((res) =>{
-                console.log(res.data);
-            }).catch((e) => {
-                if (axios.isCancel(e)) return
-            })
+            // cancelToken : new axios.CancelToken((c) => {
+            //     cancel = c
+            // }).then((res) =>{
+            //     console.log(res.data);
+            // }).catch((e) => {
+            //     if(axios.isCancel(e)){
+            //         return e;
+            //     }
+            // })
     }) , [query, pageNumber]);
     return(<div>Hello</div>);
 }
